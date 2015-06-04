5d2884181d57746a94409143d71dd0859cf4a79b
Brad Serbu <bradserbu@bradserbu.com>
2015-06-04 18:35:33 -0400

    Working on a Job Builder

---
+++ src/mylonite.core/JobBuilder.cs
@@ -0,0 +1,32 @@
+﻿using mylonite.core.interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mylonite.core
+{
+    public class JobBuilder : Component
+    {
+        public JobBuilder(string jobName)
+        {
+            this.JobName = JobName;
+            this.m_activities = new List<IActivity>();
+        }
+
+        List<IActivity> m_activities;
+
+        public string JobName { get; private set; }
+
+        public void Do(Action action)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Do(IActivity activity, Func<IArgument, IValue> getArgumentValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
